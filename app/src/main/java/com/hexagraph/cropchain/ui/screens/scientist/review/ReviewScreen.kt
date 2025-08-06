dd76c2a99071545302eb192d0a38c366646aa1ce
harsh-bhadauria <hsb.harsh070@gmail.com>
2025-08-06 11:45:28 +0530

    smol visual thing

--- app/src/main/java/com/hexagraph/cropchain/ui/screens/scientist/review/ReviewScreen.kt
+++ app/src/main/java/com/hexagraph/cropchain/ui/screens/scientist/review/ReviewScreen.kt
@@ -12,12 +12,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -29,11 +25,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.hexagraph.cropchain.R
 import com.hexagraph.cropchain.ui.component.DisplayImageList
 
 @Composable
@@ -127,7 +121,6 @@ fun UpperScreen(onTabSelected: (String) -> Unit, selectedTab: String) {
 
             }
         }
-
-        Spacer(modifier = Modifier.height(8.dp))
+//        Spacer(modifier = Modifier.height(8.dp))
     }
 }
